a48c7ef9acdac63087edda123d2810a696a8b338
Ryoung27 <RRYoung89@gmail.com>
2018-09-27 17:53:28 -0500

    done with chapter two

--- chapterTwo.js
+++ chapterTwo.js
@@ -24,3 +24,60 @@ console.log(greeting + name);
 
 let x = 30;
 console.log("The value of x ", x);
+
+console.log(Math.max(2,4));
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " + theNumber * theNumber)
+
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+    console.log("small");
+} else if (num < 100) {
+    console.log("medium")
+} else {
+    console.log("large")
+}
+
+for(let number = 0; number <= 12; number = number + 2){
+    console.log(number)
+}
+
+let result =1;
+for(let counter = 0; counter < 10; counter = counter + 1){
+    result = result * 2;
+}
+console.log(result)
+
+counter = counter + 1
+
+counter += 1
+
+hash = "#"
+hashTwo = "#"
+
+for(let number = 0; number < 7; number++){
+    console.log(hash)
+    hash = hash + hashTwo
+}
+
+for(let number = 0; number < 101; number ++) {
+    if(number % 5 == 0 && number % 3 == 0){
+       console.log("Fizzbuzz")
+    }else if(number % 5 == 0){
+       console.log("Fizz")
+    }else if(number % 3 == 0){
+       console.log("Buzz")
+    }else{
+       console.log(number)
+        }
+   }
+
+for(let number = 0; number < 8; number++){
+    if(number % 2 == 0){
+        console.log(" # # # #")
+    }else {
+        console.log("# # # # ")
+    }
+}
