ee459a8d4aaa40e5e8966632a3b57b12fb703d9a
Ryoung27 <RRYoung89@gmail.com>
2018-09-28 08:28:08 -0500

    chapter four objects

---
+++ chapterFour.js
@@ -0,0 +1,16 @@
+//Manipulation of data
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+console.log(listOfNumbers[0]);
+console.log(listOfNumbers[2 -1]);
+
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
