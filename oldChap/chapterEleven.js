3d7248e1d76d872575e414abad10045a37b4b305
Ryoung27 <RRYoung89@gmail.com>
2018-10-01 06:15:38 -0500

    chapter eleven

---
+++ oldChap/chapterEleven.js
@@ -0,0 +1,37 @@
+//Async
+//Programming with multiple threads means using async programming
+// to run multiple threads of programs at once
+
+class Timeout extends Error {}
+
+function request(nest, target, type, content) {
+  return new Promise((resolve, reject) => {
+    let done = false;
+    function attempt(n) {
+      nest.send(target, type, content, (failed, value) => {
+        done = true;
+        if (failed) reject(failed);
+        else resolve(value);
+      });
+      setTimeout(() => {
+        if (done) return;
+        else if (n < 3) attempt(n + 1);
+        else reject(new Timeout("Timed out"));
+      }, 250);
+    }
+    attempt(1);
+  });
+}
+
+function requestType(name, handler) {
+    defineRequestType(name, (nest, content, source,
+                             callback) => {
+      try {
+        Promise.resolve(handler(nest, content, source))
+          .then(response => callback(null, response),
+                failure => callback(failure));
+      } catch (exception) {
+        callback(exception);
+      }
+    });
+  }
